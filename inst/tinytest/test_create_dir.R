8217d1b170bc5b47a9da6722289f986ff2b80d07
JesseAlderliesten <jessealderliesten@hotmail.com>
2025-12-27 12:43:42 +0100

    create_dir(): simplify by using file.path() in the default value for 'dir' and removing argument 'subdir'. Added suggestions to create temporary directories.

--- inst/tinytest/test_create_dir.R
+++ inst/tinytest/test_create_dir.R
@@ -1,106 +1,129 @@
 #### To do ####
-# tinytest supports tracking of side-effects, but I'm not yet sure how to
-# properly use it. E.g.,
-# m <- tinytest::run_test_file(file = "inst/tinytest/test_create_dir.R",
-#                              side_effects = TRUE)
-# tinytest::run_test_file(file = "inst/tinytest/test_create_dir.R",
-#   side_effects = report_side_effects(report = FALSE, envvar = FALSE, pwd = TRUE,
-#                                      files = TRUE, locale = FALSE))
-# tinytest::report_side_effects(report = FALSE)
-
-# Test creation of files in a temporary folder that is automatically destroyed
-# after the test is run. See the examples in ?run_test_dir() and ?run_test_file()
-# and see https://stackoverflow.com/questions/17107206/change-temporary-directory
-
-
-#### Create objects to use in tests ####
-warning("Not yet implemented all tests for create_dir()!")
+# - tinytest supports tracking of side-effects, but I'm not yet sure how to
+#   properly use it. E.g.,
+#   m <- tinytest::run_test_file(file = "inst/tinytest/test_create_dir.R",
+#                                side_effects = TRUE)
+#   tinytest::run_test_file(file = "inst/tinytest/test_create_dir.R",
+#                           side_effects = report_side_effects(
+#                             report = FALSE, envvar = FALSE, pwd = TRUE,
+#                             files = TRUE, locale = FALSE))
+#   tinytest::report_side_effects(report = FALSE)
+# - Create test files in a temporary folder that is (automatically or
+#   programmatically) removed after the test is run. See ?tinytest::run_test_dir(),
+#   ?tinytest::run_test_file(), ?tempdir, ?tempfile, ?unlink, ?files,
+#   https://github.com/r-lib/testthat/issues/664, and
+#   https://stackoverflow.com/questions/17107206/change-temporary-directory
+#   And the code in https://stat.ethz.ch/pipermail/r-package-devel/2024q4/011196.html:
+#   "I think the idea is to do as below. You can also make temporary directories
+#   in a similar way."
+#     tempFile <- tempfile()
+#     # write to tempFile, and use it anyway you like
+#     unlink(tempFile)
 
 #### Test the examples ####
+warning("Not yet tested examples of create_dir()!")
 
-
-#### Test other sections ####
 
 #### Tests ####
+# 1a without date directory
+dir <- "temp_subdirF_dateF"
+expected_path <- dir
+expect_message(
+  expect_identical(
+    create_dir(dir = dir, add_date = FALSE),
+    expected_path),
+  pattern = paste0("Created directory.+", expected_path), strict = TRUE)
+expect_true(dir.exists(expected_path))
 
-# 1a no subdirectory, with date directory
+# 1b without date directory, directory already exists
+expect_message(
+  expect_identical(
+    create_dir(dir = dir, add_date = FALSE),
+    expected_path),
+  pattern = paste0(expected_path, ".+already exists"), strict = TRUE)
+expect_true(dir.exists(expected_path))
+
+# 2a with date directory
 dir <- "temp_subdirF_dateT"
 expected_path <- file.path(dir, format(Sys.time(), format = "%Y_%m_%d"))
 expect_message(
   expect_identical(
-    create_dir(dir = dir, subdir = "", add_date = TRUE),
+    create_dir(dir = dir, add_date = TRUE),
     expected_path),
   pattern = paste0("Created directory.+", expected_path), strict = TRUE)
 expect_true(dir.exists(expected_path))
 
-# 1b no subdirectory, with date directory, directory already exists
+# 2b with date directory, directory already exists
 expect_message(
   expect_identical(
-    create_dir(dir = dir, subdir = "", add_date = TRUE),
+    create_dir(dir = dir, add_date = TRUE),
     expected_path),
   pattern = paste0(expected_path, ".+already exists"), strict = TRUE)
 expect_true(dir.exists(expected_path))
 
-# 2a with subdirectory, with date directory
+# 3a with subdirectories, with date directory
 dir <- "temp_subdirT_dateT"
 expected_path <- file.path(dir, "subdir", format(Sys.time(), format = "%Y_%m_%d"))
 expect_message(
   expect_identical(
-    create_dir(dir = dir, subdir = "subdir", add_date = TRUE),
+    create_dir(dir = file.path(dir, "subdir"), add_date = TRUE),
     expected_path),
   pattern = paste0("Created directory.+", expected_path), strict = TRUE)
 expect_true(dir.exists(expected_path))
 
-# 2b with subdirectory, with date directory, directory already exists
+# 3b with subdirectories, with date directory, directory already exists
 expect_message(
   expect_identical(
-    create_dir(dir = dir, subdir = "subdir", add_date = TRUE),
+    create_dir(dir = file.path(dir, "subdir"), add_date = TRUE),
     expected_path),
   pattern = paste0(expected_path, ".+already exists"), strict = TRUE)
 expect_true(dir.exists(expected_path))
 
-# 2c 'dir' and 'subdir' contains slashes to indicate subdirectories, with date
-# directory
-dir <- file.path("temp_dirsubs_subdirT_dateT", "sub1", "sub2")
-expected_path <- file.path(dir, "subdir", "sub3", "sub4",
-                           format(Sys.time(), format = "%Y_%m_%d"))
+# 4a "." should create the subfolder with the current date in the working directory
+dir <- "."
+expected_path <- paste0(dir, .Platform$file.sep,
+                        format(Sys.time(), format = "%Y_%m_%d"))
 expect_message(
   expect_identical(
-    create_dir(dir = dir, subdir = file.path("subdir", "sub3", "sub4"),
-               add_date = TRUE),
+    create_dir(dir = dir, add_date = TRUE),
     expected_path),
-  pattern = paste0("Created directory.+", expected_path), strict = TRUE)
-expect_true(dir.exists(expected_path))
+  pattern = paste0("Created directory '", expected_path), strict = TRUE)
+expect_true(dir.exists(file.path(expected_path)))
+expect_true(dir.exists(file.path(getwd(), format(Sys.time(), format = "%Y_%m_%d"))))
 
-# 3a checks on input to 'dir'
+# 4b further checks on input to 'dir'
 for(dir in list(3, "", character(0), NULL, c("temp_p1", "temp_p2"))) {
   expect_error(
-    create_dir(dir = dir, subdir = ""),
+    create_dir(dir = dir),
     pattern = "is_character(dir) is not TRUE", fixed = TRUE)
 }
 
-# 3b checks on input to 'subdir'
-for(subdir in list(3, character(0), NULL, c("temp_p1", "temp_p2"))) {
+for(dir in list("./", "temp_p1/")) {
   expect_error(
-    create_dir(dir = ".", subdir = subdir),
-    pattern = "is_character(subdir, allow_empty = TRUE) is not TRUE", fixed = TRUE)
+    create_dir(dir = dir),
+    pattern = "'dir' should not end with '/'", fixed = TRUE)
 }
 
-# 4a further checks on input to 'dir'
-for(dir in list("./", ".\\", "temp_p1/", "temp_p1\\")) {
+for(dir in list(".\\", "temp_p1\\")) {
   expect_error(
-    create_dir(dir = dir, subdir = ""),
-    pattern = "is_character(dir) is not TRUE", fixed = TRUE)
+    create_dir(dir = dir),
+    pattern = "'dir' should not end with '\\'", fixed = TRUE)
 }
 
-# 4b further checks on input to 'subdir'
-for(subdir in list(character(0), NULL, c("temp_p1", "temp_p2"))) {
+# 5 Checks on input to 'add_date'
+for(add_date in list(3, NA)) {
   expect_error(
-    create_dir(dir = ".", subdir = subdir),
-    pattern = "is_character(subdir, allow_empty = TRUE) is not TRUE", fixed = TRUE)
+    create_dir(dir = ".", add_date = add_date),
+    pattern = "is_logical(add_date) is not TRUE", fixed = TRUE)
 }
 
-
+# 6 Checks on input to 'quietly'
+for(quietly in list(3, NA)) {
+  expect_error(
+    create_dir(dir = ".", quietly = quietly),
+    pattern = "is_logical(quietly) is not TRUE", fixed = TRUE)
+}
 
 
 #### Remove objects used in tests ####
+rm(add_date, dir, expected_path, quietly)
