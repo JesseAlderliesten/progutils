517ea3bc41ff196b6183316c62f48f79e9eb0df2
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-01-03 11:53:34 +0100

    bugfix numvect_to_char(): return character vector instead of character string if collapse = NULL, with an example. In addition, check that 'signif' is positive instead of nonnegative and add example of using numvect_to_char() on character data.

--- R/numvect_to_char.R
+++ R/numvect_to_char.R
@@ -5,8 +5,8 @@
 #'
 #' @param x A vector or a non-dataframe list that can be unlisted to obtain a
 #' vector.
-#' @param signif Positive integer of length one indicating the number of
-#' significant digits to round numeric `x` to.
+#' @param signif Positive number of length one, rounded to the nearest positive
+#' integer indicating the number of significant digits to round numeric `x` to.
 #' @param sep Character string of length one used to separate the names and the
 #' values.
 #' @param collapse Character string of length one to collapse values into a
@@ -40,12 +40,16 @@
 #' x <- 1:3
 #' names(x) <- letters[x]
 #' numvect_to_char(x = x) # "a: 1, b: 2, c: 3"
+#' numvect_to_char(x = x, collapse = NULL) # c("a: 1", "b: 2", "c: 3")
 #' numvect_to_char(x = unname(x)) # "1, 2, 3"
-#' y <- x
-#' y <- y / 7
+#' y <- x / 7
 #' numvect_to_char(x = y, signif = 7) # "a: 0.1428571, b: 0.2857143, c: 0.4285714"
 #' numvect_to_char(x = y, signif = 2, sep = " = ", collapse = " and ", width = 15)
 #' # "a = 0.14 and b\n= 0.29 and c =\n0.43"
+#'
+#' x_char <- c(a = "abc", b = "def", c = "this is text")
+#' numvect_to_char(x = x_char) # "a: abc, b: def, c: this is text"
+#' numvect_to_char(x = unname(x_char)) # "abc, def, this is some text"
 #'
 #' # Showing the use of numvect_to_char to get a frequency table
 #' x <- 1:10
@@ -58,7 +62,7 @@
 #' @export
 numvect_to_char <- function(x, signif = 3L, sep = ": ", collapse = ", ",
                             width = Inf) {
-  stopifnot(checkinput::is_nonnegative(signif),
+  stopifnot(checkinput::is_positive(signif), checkinput::is_character(sep),
             is.null(collapse) || checkinput::is_character(collapse),
             checkinput::is_positive(width))
   if(is.list(x) && !is.data.frame(x)) {
@@ -73,9 +77,13 @@ numvect_to_char <- function(x, signif = 3L, sep = ": ", collapse = ", ",
 
   if(checkinput::all_characters(x = names(x), allow_empty = TRUE,
                                 allow_zero = TRUE, allow_NA = TRUE)) {
-    out_char <- paste(names(x), x, sep = sep, collapse = collapse)
+    x <- paste(names(x), x, sep = sep, collapse = collapse)
   } else {
-    out_char <- paste(x, collapse = collapse)
+    x <- paste(x, collapse = collapse)
   }
-  wrap_text(x = out_char, width = width)
+
+  # Not calling wrap_text() directly because it would paste the character vector
+  # obtained for 'collapse = NULL' into a single string.
+  vapply(X = x, FUN = wrap_text, FUN.VALUE = character(1L), width = width,
+         USE.NAMES = FALSE)
 }
