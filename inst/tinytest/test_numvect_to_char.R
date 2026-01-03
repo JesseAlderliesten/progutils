6ae13f6e998f20fe47b5e0ea1ec7b22d8b494862
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-01-03 12:03:18 +0100

    Test character input, 'collapse = NULL', and erroneous input to arguments.

--- inst/tinytest/test_numvect_to_char.R
+++ inst/tinytest/test_numvect_to_char.R
@@ -1,8 +1,7 @@
 #### Create objects to use in tests ####
 a <- 1:3
 names(a) <- letters[a]
-b <- a
-b <- b / 7
+b <- a / 7
 c <- 1:10
 names(c) <- letters[c]
 d <- 5:15
@@ -10,12 +9,15 @@ names(d) <- letters[d]
 
 x_in <- c(am = -1, bm = -2, i = 9, j = 10, z = 26, a = 1, b = 2, c = 3)
 x_in_InfNa <- c(x_in[1:3], l = NA, m = NaN, n = Inf, o = -Inf, x_in[4:8])
+x_in_char <- c(a = "abc", b = "def", c = "this is text")
 x_out_named <- paste(names(x_in), x_in, sep = ": ", collapse = ", ")
 x_out_nonint <- paste(signif(x = x_in / 8, digits = 2), collapse = ", ")
+x_out_char <- "a: abc, b: def, c: this is text"
 
 
 #### Test the examples ####
 expect_identical(numvect_to_char(x = a), "a: 1, b: 2, c: 3")
+expect_identical(numvect_to_char(x = a, collapse = NULL), c("a: 1", "b: 2", "c: 3"))
 expect_identical(numvect_to_char(x = unname(a)), "1, 2, 3")
 
 expect_identical(
@@ -23,6 +25,9 @@ expect_identical(
 expect_identical(
   numvect_to_char(x = b, signif = 2, sep = " = ", collapse = " and ", width = 15),
   "a = 0.14 and b\n= 0.29 and c =\n0.43")
+
+expect_identical(numvect_to_char(x = x_in_char), x_out_char)
+expect_identical(numvect_to_char(x = unname(x_in_char)), "abc, def, this is text")
 
 expect_identical(
   paste0(numvect_to_char(c(table(c(c, d))), sep = " (", collapse =  "), "), ")"),
@@ -33,6 +38,9 @@ expect_identical(
 #### Tests ####
 expect_identical(numvect_to_char(x = x_in), x_out_named)
 expect_identical(numvect_to_char(x = unname(x_in)), paste(x_in, collapse = ", "))
+
+expect_identical(numvect_to_char(x = x_in_char), x_out_char)
+expect_identical(numvect_to_char(x = unname(x_in_char)), "abc, def, this is text")
 
 expect_identical(
   numvect_to_char(x = x_in / 8, signif = 2L),
@@ -50,6 +58,12 @@ expect_identical(
        x = gsub(pattern = ": ", replacement = "=", x = x_out_named, fixed = TRUE),
        fixed = TRUE))
 
+expect_identical(
+  numvect_to_char(x = x_in_char, sep = "=", collapse = " & "),
+  gsub(pattern = ", ", replacement = " & ",
+       x = gsub(pattern = ": ", replacement = "=", x = x_out_char, fixed = TRUE),
+       fixed = TRUE))
+
 expect_identical(numvect_to_char(x = x_in_InfNa),
                  paste(names(x_in_InfNa), x_in_InfNa, sep = ": ", collapse = ", "))
 
@@ -63,6 +77,38 @@ expect_warning(expect_identical(
          " a: 1, b: 2, c: 3, a: 0.14, b: 0.29, c: 0.43, : 0.14, : 0.29, : 0.43")),
   pattern = "Unlisted 'x' to obtain a vector!", strict = TRUE)
 
+expect_error(numvect_to_char(x = data.frame(a = 1:3)),
+             pattern = "is.vector(x) is not TRUE", fixed = TRUE)
+
+expect_error(numvect_to_char(x = x_in, signif = 0L),
+             pattern = "checkinput::is_positive(signif) is not TRUE", fixed = TRUE)
+
+expect_error(numvect_to_char(x = x_in, signif = c(3L, 4L)),
+             pattern = "checkinput::is_positive(signif) is not TRUE", fixed = TRUE)
+
+expect_error(numvect_to_char(x = x_in, sep = c(":", "=")),
+             pattern = "checkinput::is_character(sep) is not TRUE", fixed = TRUE)
+
+expect_error(numvect_to_char(x = x_in, sep = NULL),
+             pattern = "checkinput::is_character(sep) is not TRUE", fixed = TRUE)
+
+expect_error(numvect_to_char(x = x_in, sep = 3),
+             pattern = "checkinput::is_character(sep) is not TRUE", fixed = TRUE)
+
+expect_error(numvect_to_char(x = x_in, collapse = c(":", "=")),
+             pattern = "checkinput::is_character(collapse) is not TRUE", fixed = TRUE)
+
+expect_error(numvect_to_char(x = x_in, collapse = 3),
+             pattern = "checkinput::is_character(collapse) is not TRUE", fixed = TRUE)
+
+expect_silent(expect_identical(
+  numvect_to_char(x = x_in / 8, signif = 2, collapse = NULL),
+  paste(names(x_in), signif(x = x_in / 8, digits = 2), sep = ": ", collapse = NULL)))
+
+expect_silent(expect_identical(
+  numvect_to_char(x = unname(x_in / 8), signif = 2L, collapse = NULL),
+  paste(signif(x = x_in / 8, digits = 2), collapse = NULL)))
+
 
 #### Remove objects used in tests ####
-rm(a, b, c, d, x_in, x_in_InfNa, x_out_named, x_out_nonint)
+rm(a, b, c, d, x_in, x_in_char, x_in_InfNa, x_out_char, x_out_named, x_out_nonint)
