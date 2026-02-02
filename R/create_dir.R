60c5a36f17cfc57645c71d819db5f1c5ea003377
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-02-02 19:07:06 +0100

    Major update to documentation. Alwas normalise input. Not issue a special warning if 'dir' points to a file instead of a repository.

--- R/create_dir.R
+++ R/create_dir.R
@@ -3,51 +3,43 @@
 #' Create a directory if it does not yet exist.
 #'
 #' @param dir Non-empty character string containing the path to a directory.
-#' Case-insensitive matching is used to checking if it already exists.
 #' @param add_date `TRUE` or `FALSE`: create a subdirectory with the current
-#' date in the format `YYYY_MM_DD`?
+#' date (format: `YYYY_MM_DD`)?
 #' @param quietly `TRUE` or `FALSE`: suppress the message indicating which
 #' directory was created?
 #'
 #' @details
-#' The `"."` in the default for `dir` indicates the [working directory][getwd()].
-#' Using [file.path()] to create `dir` ensures the correct (platform-dependent)
-#' file separator is used to indicate subdirectories.
+#' Using [file.path()] ensures the correct (platform-dependent) file separator
+#' is used to indicate subdirectories. The `"."` in the default for `dir`
+#' indicates the [working directory][getwd()].
 #'
 #' Several limitations are imposed on `dir` to facilitate handling of paths by
 #' Windows, see [dir.create()]: `dir` should not end in a slash, backslash, or
-#' space, which would be removed when the [directory is created][dir.create()],
+#' space because those characters would be removed when the directory is created,
 #' leading to a mismatch between the created directory and the returned path.
 #' `dir` should also not end in a dot. Finally, `dir` should not contain the
 #' characters `"`, `*`, `?`, `|`, `<`, or `>`.
 #'
 #' @returns
-#' A character string with the [normalized][normalizePath()] form of the
-#' created path, returned [invisibly][invisible]. The path will be composed of
-#' `dir`, and, if `add_date` is `TRUE`, a subdirectory with the current date in
-#' the format `YYYY_MM_DD`. The [working directory][getwd()] is returned if the
-#' attempt to create a directory fails, with a warning.
+#' A character string with the [normalized][normalizePath()] path to the
+#' requested directory, returned [invisibly][invisible]. The
+#' [working directory][getwd()] is returned if an attempt to create a directory
+#' fails, with a warning.
 #'
 #' @section Side effects:
 #' The requested directory is created if does not yet exist.
 #'
-#' @section To do:
-#' - What happens if 'dir' points to an existing file instead of to an existing
-#'   directory?
-#' - Why are paths only [normalized][normalizePath()] to use in messages, not
-#'   before checking existence?
-#'
 #' @seealso
-#' [dir.exists()] and [dir.create()] used by this function; [check_file()] to
-#' check if a file exists and is a unique match to a pattern; [file.path()] to
-#' construct the path to a file from components in a platform-independent way;
-#' [normalizePath()] to create absolute paths.
+#' [check_file()] to check if a file exists and is a unique match to a pattern;
+#' [file.path()] to construct file paths in a platform-independent way;
+#' [normalizePath()] to create absolute paths; [dir.exists()] and [dir.create()]
+#' used by this function.
 #'
 #' `fs::path_sanitize()` to *remove* invalid characters from potential paths,
 #' looking for a wider range of invalid characters.
 #'
 #' @family
-#' functions to check and modify paths and directories
+#' functions to check paths and create directories
 #'
 #' @examples
 #' # Use a temporary directory to not write in the user's directory
@@ -101,8 +93,10 @@ create_dir <- function(dir = file.path(".", "output"), add_date = TRUE,
     dir <- file.path(dir, format(Sys.time(), format = "%Y_%m_%d"))
   }
 
+  dir <- normalizePath(dir, mustWork = FALSE)
+
+  # dir.exists() returns FALSE if 'dir' is a file instead of a directory.
   if(dir.exists(dir)) {
-    dir <- normalizePath(dir)
     if(!quietly) {
       message("Directory '", dir, "' already exists.")
     }
@@ -114,17 +108,15 @@ create_dir <- function(dir = file.path(".", "output"), add_date = TRUE,
     # - Using 'recursive = TRUE' to allow creation of subdirectories inside a
     #   not-yet existing directory (e.g., creating './output/<date>' if
     #   './output' does not yet exist).
-    if(dir.create(path = dir, recursive = TRUE)) {
-      dir <- normalizePath(dir)
+    if(dir.create(path = dir, recursive = TRUE, showWarnings = FALSE)) {
       if(!quietly) {
         message("Created directory '", dir, "'.")
       }
     } else {
       warning(wrap_text(paste0(
-        "Attempt to create directory '", normalizePath(dir), "' failed",
-        if(file.exists(dir)) {": it points to a file, not to a directory"},
+        "Attempt to create directory '", dir, "' failed",
         "!\nReturning the working directory ('", getwd(), "') instead.")))
-      dir <- normalizePath(getwd())
+      dir <- normalizePath(getwd(), mustWork = NA)
     }
   }
 
