218b609087287ae8e8309bb964b0526a88e43d7e
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-02-03 13:24:50 +0100

    Major update: add argument 'paths', also consider 'x.y' an R-version.

--- R/get_paths.R
+++ R/get_paths.R
@@ -1,15 +1,21 @@
 #' Get paths to directories
 #'
-#' Wrapper around [.libPaths()] to get the paths \R knows about, with paths
-#' containing the same \R [version number][getRversion()] (`x.y.z`) as the
-#' currently running \R session given before other paths.
+#' Get the paths \R knows about, with paths containing the currently used \R
+#' version number given before other paths.
+#'
+#' @param paths The paths to use.
+#'
+#' @details
+#' This is a wrapper around [.libPaths()] to first return paths containing the
+#' currently used \R [version number][getRversion()] in the form of directory
+#' `R-x.y.z` or directory `x.y`, with a warning if none such path is present.
+#'
+#' A warning is also issued if the returned vector is of length zero or only
+#' contains empty paths (`""`).
 #'
 #' @returns
 #' A character vector containing the same elements as [.libPaths()], possibly in
 #' a different order.
-#'
-#' @section Programming note:
-#' See `InstallPkgs::prepare_install()` where `get_paths()` is checked and used.
 #'
 #' @family
 #' functions to check paths and create directories
@@ -20,12 +26,24 @@
 #' getRversion() # The currently used R version
 #'
 #' @export
-get_paths <- function() {
-  paths_libPaths <- .libPaths()
-  if(length(paths_libPaths) == 0L || all(!nzchar(paths_libPaths))) {
-    warning("'.libPaths' does not contain any non-empty path!")
+get_paths <- function(paths = .libPaths()) {
+  if(length(paths) == 0L || all(!nzchar(paths))) {
+    warning("'.libPaths()' did not return any non-empty paths!")
+    return(paths)
   }
-  bool_Rversion <- grepl(pattern = paste0("R-", as.character(getRversion())),
-                         x = paths_libPaths, fixed = TRUE)
-  c(paths_libPaths[bool_Rversion], paths_libPaths[!bool_Rversion])
+
+  current_full <- paste0("R-", as.character(getRversion()))
+  current_minor <- substr(x = current_full, start = 3, stop = 5)
+  # .libPaths() uses normalizePath(winslash = "/") instead of the default "\\"
+  bool_current <- grepl(pattern = paste0("/", current_full, "|/", current_minor),
+                        x = paths)
+
+  if(!any(bool_current)) {
+    warning(wrap_text(paste0(
+      "The currently used R version (", substring(current_full, first = 3),
+      ") is not present as directory '", current_full, "' or '", current_minor,
+      "' in any of the retrieved paths: ", paste_quoted(unname(paths)), "!")))
+  }
+
+  c(paths[bool_current], paths[!bool_current])
 }
