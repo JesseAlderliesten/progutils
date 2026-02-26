3b56c4b0dacd7ddcd1e9b2c081429d324be0bde8
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-02-26 12:52:49 +0100

    Use `winslash = "/"` to normalise paths. Error if filename contains (back)slashes. Also check stamp. Added examples and tests.

--- R/create_path.R
+++ R/create_path.R
@@ -7,96 +7,134 @@
 #' extension like `.csv` or `.txt`.
 #' @param format_stamp A character string indicating the [format][strftime()] of
 #' the stamp to be added in front of the file name. No stamp is added if
-#' `format_stamp` is an empty string (i.e., `""`).
+#' `format_stamp` is an empty string (i.e., `""`). The formatted stamp is
+#' treated as part of the filename, such that the same restrictions apply, see
+#' `Details`.
 #'
-#' @details
-#' `filename` should contain a file extension: a dot followed by alphanumeric
-#' characters until the end of the file name. An error occurs if no file
-#' extension is present. Non-alphanumeric characters in `filename` before the
-#' file extension are replaced by underscores, with a warning.
-#'
-#' A warning is issued if the file indicated by the returned path already exists.
-#' Use `"%OSn"` as part of `format_stamp` to truncate seconds to `0 <= n <= 6`
-#' decimal places for precise stamps, see [strftime()] for details.
-#'
-#' The directory for the returned path is [created][create_dir()] if it does not
-#' yet exist. By default it is a subdirectory with the current date in format
-#' `YYYY_mm_dd` in directory `output` below the current working directory.
-#'
-#' The returned path is [normalised][normalizePath()] such that it still works
-#' if the [working directory][getwd()] changes. `"/"` instead of `"\\"` is used
-#' as [winslash][normalizePath()], such that the returned path can be used in
-#' Windows' file system.
+#' @inherit create_dir details
 #'
 #' @returns
 #' The created file path, returned [invisibly][invisible()].
+#'
+#' @details
+#' `filename` should contain a file extension (i.e., a dot followed by
+#' alphanumeric characters until the end of the file name). It should not
+#' contain slashes or backslashes: use `dir` to indicate (sub)directories.
+#' Non-alphanumeric characters other than underscores before the file extension
+#' are replaced by dots, with a warning.
+#'
+#' The default `dir` is a subdirectory with the current date in the
+#' [format][strftime()] `YYYY_mm_dd` in directory `output` below the working
+#' directory. [file.path()] ensures the correct ([platform][.Platform]-dependent)
+#' file separator is used to indicate subdirectories, and `"."` indicates the
+#' [working directory][getwd()].
+#'
+#' Various restrictions are imposed on `dir`, see [create_dir()].
+#'
+#' The absolute [normalised][normalizePath()] path is returned such that the
+#' returned path still works if the [working directory][getwd()] changes. `"/"`
+#' instead of `"\\"` is used as argument [winslash][normalizePath()] such that
+#' the returned path can be used in Windows' file system.
+#'
+#' The *directory* for the returned path is [created][create_dir()] if it does
+#' not yet exist. A warning is issued if the *file* indicated by the returned
+#' path already exists. Use `"%OSn"` as part of `format_stamp` to create precise
+#' stamps by truncating seconds to `0 <= n <= 6` decimal places to prevent this,
+#' see [strftime()] for details.
 #'
 #' @section Side effects:
 #' The directory indicated by the returned file path is created if it does not
 #' yet exist.
 #'
 #' @section Programming notes:
-#' File names that end in a dot are handled incorrectly by `create_path()`:
-#' [tools::file_path_sans_ext()] does not recognise the extension of file names
-#' that end in a dot, whereas [tools::file_ext()] does recognise such extension.
-#' Therefore, `create_path("ff..txt")` produces the nonsense result ending in
-#' `"ff__txt.txt"` instead of ending in `"ff_.txt"`.
+#' [tools::file_path_sans_ext()] does *not* recognise the extension of file names
+#' that end in a dot, whereas [tools::file_ext()] *does* recognise such
+#' extensions. Using [progutils::file_path_sans_ext()] from `progutils` prevents
+#' problems caused by this discrepancy, see the `Examples` of
+#' [progutils::file_path_sans_ext()] from `progutils`.
 #'
 #' @seealso
 #' [get_filename()] to check if a file exists and is a unique match to a pattern;
 #' [file.path()] to construct file paths in a platform-independent way;
-#' [normalizePath()] to create absolute paths; [create_dir()] used by this
-#' function to create a directory if it does not yet exists.
+#' [normalizePath()] to create absolute normalised paths; [create_dir()] to
+#' create a directory if it does not yet exists.
 #'
 #' @family
 #' functions to check paths and create directories
 #'
 #' @examples
+#' # Use a temporary directory to not write in the user's directory
+#' my_tempdir <- normalizePath(path = tempdir(), winslash = "/", mustWork = NA)
+#'
+#' (create_path(filename = "abc.txt", format_stamp = "",
+#'             dir = my_tempdir, add_date = TRUE))
+#' (create_path(filename = "abc.txt", format_stamp = "%d_%m_%Y",
+#'             dir = my_tempdir, add_date = TRUE))
+#' (create_path(filename = "def.html", format_stamp = "",
+#'             dir = my_tempdir, add_date = FALSE))
+#' (create_path(filename = "def.html", format_stamp = "%d_%m_%Y",
+#'             dir = my_tempdir, add_date = FALSE))
+#' (create_path(filename = "abc.txt", format_stamp = "",
+#'             dir = file.path(my_tempdir, "subdir"), add_date = TRUE))
+#' (create_path(filename = "abc.txt", format_stamp = "%d_%m_%Y",
+#'             dir = file.path(my_tempdir, "subdir"), add_date = TRUE))
+#' (create_path(filename = "def.html", format_stamp = "",
+#'             dir = file.path(my_tempdir, "subdir"), add_date = FALSE))
+#' (create_path(filename = "def.html", format_stamp = "%d_%m_%Y",
+#'             dir = file.path(my_tempdir, "subdir"), add_date = FALSE))
+#'
+#' # Cleaning up
+#' unlink(x = file.path(my_tempdir,
+#'                      c(format(Sys.time(), format = "%Y_%m_%d"), "subdir")),
+#'        recursive = TRUE)
+#' rm(my_tempdir)
 #'
 #' @export
 create_path <- function(filename, format_stamp = "%Y_%m_%d_%H_%M_%S",
                         dir = file.path(".", "output"), add_date = TRUE) {
   stopifnot(checkinput::is_character(filename),
-            checkinput::is_character(format_stamp, allow_empty = TRUE))
+            checkinput::is_character(format_stamp, allow_empty = TRUE),
+            checkinput::is_character(dir))
 
   file_ext <- tools::file_ext(filename)
-  if(file_ext == "") {
+  # See the 'Programming note' why not using tools::file_path_sans_ext()
+  file_sans_ext <- progutils::file_path_sans_ext(filename)
+  if(file_ext == "" || file_sans_ext == "" || filename == file_sans_ext) {
     stop(wrap_text(paste0(
-      "Filename '", filename, "' should contain a file extension: a dot followed",
-      " by only alphanumeric characters until the end of the file name.")))
+      "'filename' should include the name and the file extension: ", filename)))
   }
 
-  file_sans_ext <- tools::file_path_sans_ext(filename)
+  if(format_stamp != "") {
+    file_stamp <- paste0(format(Sys.time(), format = format_stamp), "_")
+    filename <- paste0(file_stamp, filename)
+    file_sans_ext <- paste0(file_stamp, file_sans_ext)
+  }
+
   if(grepl(pattern = "/|\\\\", x = file_sans_ext)) {
-    file_sans_ext <- gsub(pattern = "/|\\\\", replacement = "_", x = file_sans_ext)
-    warning("Replaced (back)slashes in filename '", filename, "' with",
-            " underscores!\nUse argument 'dir' to indicate the directory:\n",
-            paste0(file_sans_ext, ".", file_ext))
+    stop("Filename '", filename, "' contains (back)slashes: use argument 'dir'",
+         " to indicate the directory!")
   }
 
-  # Replace non-alphanumeric characters (including spaces) with underscores
-  file_sans_ext_vld <- gsub(pattern = "[^[:alnum:]]", replacement = "_",
+  # Replace non-alphanumeric characters other than underscores with dots.
+  file_sans_ext_vld <- gsub(pattern = "[^[:alnum:]_]", replacement = ".",
                             x = file_sans_ext)
   filename_vld <- paste0(file_sans_ext_vld, ".", file_ext)
   if(file_sans_ext != file_sans_ext_vld) {
-    warning(progutils::wrap_text(paste0(
-      "Replaced non-alphanumeric characters in filename '", filename,
-      "' with underscores!")))
+    warning("Replaced non-alphanumeric characters other than underscores in",
+            " filename\n'", filename, "' with dots: ", filename_vld)
   }
 
-  # See section 'Details'
-  file_path <- normalizePath(
-    path = file.path(progutils::create_dir(dir = dir, add_date = add_date),
-                     paste0(
-                       if(format_stamp != "") {
-                         paste0(format(Sys.time(), format = format_stamp), "_")
-                       }, filename_vld)
-    ),
-    winslash = "/", mustWork = FALSE)
+  file_path <- file.path(progutils::create_dir(dir = dir, add_date = add_date),
+                         filename_vld)
+  file_path <- normalizePath(path = file_path, winslash = "/", mustWork = FALSE)
+
   if(file.exists(file_path)) {
-    # To do:
-    # - Add 'call. = FALSE'?
-    warning(paste0("File already exists:\n", file_path))
+    if(dir.exists(file_path)) {
+      warning(paste0("Path already exists but points to a directory, not a file:\n",
+                     file_path))
+    } else {
+      warning(paste0("File already exists:\n", file_path))
+    }
   }
   invisible(file_path)
 }
--- inst/tinytest/test_create_path.R
+++ inst/tinytest/test_create_path.R
@@ -1,162 +1,307 @@
-warning("test_create_path.R is not yet complete!")
-
 #### Test the examples ####
-warning("Test examples of create_path()!")
-# my_tempdir <- tempdir()
-
-# Cleaning up
-# unlink(c(res_dir_one, dirname(res_dir_two)), recursive = TRUE)
-# rm(my_tempdir, res_dir_one, res_dir_one_v2, res_dir_one_v3, res_dir_two)
+# Examples are tested in section 'Correct combinations' below
 
 
 #### Tests ####
-my_tempdir <- tempdir()
-dir_out_p1 <- normalizePath(path = my_tempdir, winslash = "/", mustWork = FALSE)
+##### Preparations #####
+# Create temporary directory and temporary file to use in tests.
+my_tempdir <- normalizePath(path = tempdir(), winslash = "/", mustWork = FALSE)
+my_tempfile <- normalizePath(path = file.path(my_tempdir, "test_df.csv"),
+                             winslash = "/", mustWork = FALSE)
+# Write csv-file, modified from example in help(write.table)
+write.table(x = data.frame(a = "a", b = pi), file = my_tempfile)
 
-expect_error(create_path(filename = "abcd", dir = my_tempdir),
-             pattern = "Filename 'abcd' should contain a file extension",
-             fixed = TRUE)
-expect_error(create_path(filename = "abc.", dir = my_tempdir),
-             pattern = "Filename 'abc.' should contain a file extension",
-             fixed = TRUE)
-expect_error(create_path(filename = "ab.c#", dir = my_tempdir),
-             pattern = "Filename 'ab.c#' should contain a file extension",
-             fixed = TRUE)
+##### Correct combinations #####
+# Test combinations of stamp formats (absent, default or alternative format)
+# with presence or absence of a date directory and a subdirectory in 'dir'.
+expect_silent(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "",
+                dir = my_tempdir, add_date = TRUE),
+    file.path(my_tempdir, format(Sys.time(), format = "%Y_%m_%d"), "abc.txt")
+  )
+)
 
-filenm_in <- c("a/a.txt", "b\\b.txt")
-filenm_out <- c("a_a.txt", "b_b.txt")
-for(ind_filenm in seq_along(filenm_in)) {
-  expect_warning(
-    expect_equal(
-      create_path(filename = filenm_in[ind_filenm], format_stamp = "",
+expect_silent(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "%d_%m_%Y",
+                dir = my_tempdir, add_date = TRUE),
+    file.path(my_tempdir, format(Sys.time(), format = "%Y_%m_%d"),
+              paste0(format(Sys.time(), format = "%d_%m_%Y"), "_abc.txt"))
+  )
+)
+
+expect_silent(
+  expect_identical(
+    create_path(filename = "def.html", format_stamp = "",
+                dir = my_tempdir, add_date = FALSE),
+    file.path(my_tempdir, "def.html")
+  )
+)
+
+expect_silent(
+  expect_identical(
+    create_path(filename = "def.html", format_stamp = "%d_%m_%Y",
+                dir = my_tempdir, add_date = FALSE),
+    file.path(my_tempdir,
+              paste0(format(Sys.time(), format = "%d_%m_%Y"), "_def.html"))
+  )
+)
+
+expect_silent(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "",
+                dir = file.path(my_tempdir, "subdir"), add_date = TRUE),
+    file.path(my_tempdir, "subdir", format(Sys.time(), format = "%Y_%m_%d"), "abc.txt")
+  )
+)
+
+expect_silent(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "%d_%m_%Y",
+                dir = file.path(my_tempdir, "subdir"), add_date = TRUE),
+    file.path(my_tempdir, "subdir", format(Sys.time(), format = "%Y_%m_%d"),
+              paste0(format(Sys.time(), format = "%d_%m_%Y"), "_abc.txt"))
+  )
+)
+
+expect_silent(
+  expect_identical(
+    create_path(filename = "def.html", format_stamp = "",
+                dir = file.path(my_tempdir, "subdir"), add_date = FALSE),
+    file.path(my_tempdir, "subdir", "def.html")
+  )
+)
+
+expect_silent(
+  expect_identical(
+    create_path(filename = "def.html", format_stamp = "%d_%m_%Y",
+                dir = file.path(my_tempdir, "subdir"), add_date = FALSE),
+    file.path(my_tempdir, "subdir",
+              paste0(format(Sys.time(), format = "%d_%m_%Y"), "_def.html"))
+  )
+)
+
+##### filename #####
+# File names that end in a dot are handled incorrectly by
+# tools::file_path_sans_ext(), so progutils::file_path_sans_ext() is used, see
+# the 'Programming note' in create_path().
+for(filenm in c("a.txt", "e3f.txt", "ff..txt", "g_g.txt")) {
+  expect_silent(
+    expect_identical(
+      create_path(filename = filenm, format_stamp = "",
                   dir = my_tempdir, add_date = FALSE),
-      paste0(dir_out_p1, "/", filenm_out[ind_filenm])
-    ),
-    pattern = paste0("Replaced (back)slashes in filename '",
-                     filenm_in[ind_filenm], "' with underscores"),
-    strict = TRUE, fixed = TRUE)
+      normalizePath(file.path(my_tempdir, filenm),
+                    winslash = "/", mustWork = FALSE)
+    )
+  )
 }
 
-# Programming notes:
-# - File names that end in a dot are handled incorrectly by create_path():
-#   tools::file_path_sans_ext() does not recognise the extension of file names
-#   that end in a dot, whereas tools::file_ext() does recognise such extension.
-#   Therefore, create_path("ff..txt") produces the nonsense result ending in
-#   "ff__txt.txt" instead of ending in "ff_.txt".
-filenm_in <- c("c#c.txt", "d d.txt", "e.e.txt")
-filenm_out <- c("c_c.txt", "d_d.txt", "e_e.txt")
-for(ind_filenm in seq_along(filenm_in)) {
-  expect_warning(
-    expect_equal(
-      create_path(filename = filenm_in[ind_filenm], format_stamp = "",
-                  dir = my_tempdir, add_date = FALSE),
-      paste0(dir_out_p1, "/", filenm_out[ind_filenm])
-    ),
-    pattern = paste0("Replaced non-alphanumeric characters in filename '",
-                     filenm_in[ind_filenm], "' with underscores"),
-    strict = TRUE, fixed = TRUE)
-}
+##### format_stamp #####
+# Characters in 'format_stamp' not part of a conversion specification in
+# strptime are interpreted literally.
+expect_silent(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "%d_%m_%Ydef",
+                dir = my_tempdir, add_date = TRUE),
+    file.path(my_tempdir, format(Sys.time(), format = "%Y_%m_%d"),
+              paste0(format(Sys.time(), format = "%d_%m_%Y"), "def_abc.txt"))
+  )
+)
+
+# Non-alphanumeric characters other than underscores in the result of
+# 'format_stamp' are replaced by underscores.
+expect_warning(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "%d#%m_%Ydef",
+                dir = my_tempdir, add_date = TRUE),
+    file.path(my_tempdir, format(Sys.time(), format = "%Y_%m_%d"),
+              paste0(format(Sys.time(), format = "%d.%m_%Y"), "def_abc.txt"))
+  ), pattern = "Replaced non-alphanumeric characters other than underscores",
+  strict = TRUE, fixed = TRUE
+)
 
 expect_silent(
   expect_equal(
-    create_path(filename = "g_g.txt", format_stamp = "",
-                dir = my_tempdir, add_date = FALSE),
-    paste0(dir_out_p1, "/g_g.txt"))
+    nchar(basename(
+      create_path(filename = "test1c.txt", format_stamp = "%H_%M_%OS3",
+                  dir = my_tempdir, add_date = FALSE))),
+    23L
+  )
 )
 
+expect_silent(
+  expect_equal(
+    nchar(basename(
+      create_path(filename = "test1c.txt", format_stamp = "%H_%M_%OS5",
+                  dir = my_tempdir, add_date = FALSE))),
+    25L
+  )
+)
+
+##### dir #####
+# "." is the working directory
+expect_silent(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "",
+                dir = file.path(my_tempdir, "."), add_date = FALSE),
+    normalizePath(file.path(my_tempdir, ".", "abc.txt"), winslash = "/",
+                  mustWork = FALSE)
+  )
+)
+
+# 'directories' that are only working directory followed by a file extension
+# are accepted!
+expect_silent(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "",
+                dir = file.path(my_tempdir, ".txt"), add_date = FALSE),
+    normalizePath(file.path(my_tempdir, ".txt", "abc.txt"), winslash = "/",
+                  mustWork = FALSE)
+  )
+)
+
+##### Warnings #####
+# 'directories' that actually are names of existing files lead to the working
+# directory being used instead!
+expect_warning(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "",
+                dir = my_tempfile, add_date = FALSE),
+    file.path(getwd(), "abc.txt")
+  ), pattern = paste0(basename(my_tempfile), "' already exists"),
+  strict = TRUE, fixed = TRUE
+)
+
+expect_warning(
+  expect_identical(
+    create_path(filename = "abc.txt", format_stamp = "",
+                dir = my_tempfile, add_date = FALSE),
+    file.path(getwd(), "abc.txt")
+  ), pattern = "Attempt to create directory", strict = TRUE, fixed = TRUE
+)
+
+# A warning is issued if the file indicated by the returned path already exists.
+expect_warning(
+  expect_identical(
+    create_path(filename = basename(my_tempfile), format_stamp = "",
+                dir = my_tempdir, add_date = FALSE),
+    my_tempfile),
+  pattern = "File already exists:", strict = TRUE, fixed = TRUE)
+
+filenm_in <- c("c#c.txt", "d d.txt")
+filenm_out <- c("c.c.txt", "d.d.txt")
+for(ind_filenm in seq_along(filenm_in)) {
+  expect_warning(
+    expect_identical(
+      create_path(filename = filenm_in[ind_filenm], format_stamp = "",
+                  dir = my_tempdir, add_date = FALSE),
+      normalizePath(file.path(my_tempdir, filenm_out[ind_filenm]),
+                    winslash = "/", mustWork = FALSE)
+    ),
+    pattern = paste0("Replaced non-alphanumeric characters other than",
+                     " underscores in filename\n'", filenm_in[ind_filenm],
+                     "' with dots: ", filenm_out[ind_filenm]),
+    strict = TRUE, fixed = TRUE)
+}
+
+##### Check 'filename' #####
+expect_error(create_path(dir = my_tempdir),
+             pattern = "argument \"filename\" is missing, with no default",
+             fixed = TRUE)
+expect_error(create_path(filename = 123, dir = my_tempdir),
+             pattern = "checkinput::is_character(filename) is not TRUE",
+             fixed = TRUE)
+expect_error(create_path(filename = c("abc.txt", "def.txt"), dir = my_tempdir),
+             pattern = "checkinput::is_character(filename) is not TRUE",
+             fixed = TRUE)
+expect_error(create_path(filename = "", dir = my_tempdir),
+             pattern = "checkinput::is_character(filename) is not TRUE",
+             fixed = TRUE)
+
+for(filenm_in in c("abcd", "abc.", "ab.c#", ".", ".txt", ".html")) {
+  expect_error(create_path(filename = filenm_in, dir = my_tempdir),
+               pattern = paste0("'filename' should include the name and the",
+                                " file extension: ", filenm_in),
+               fixed = FALSE)
+}
+
+filenm_in <- c("a/a.txt", "b\\b.txt")
+for(ind_filenm in seq_along(filenm_in)) {
+  expect_error(
+    create_path(filename = filenm_in[ind_filenm], format_stamp = "",
+                dir = my_tempdir, add_date = FALSE),
+    pattern = paste0("Filename '",
+                     filenm_in[ind_filenm], "' contains (back)slashes"),
+    fixed = TRUE)
+}
+
+##### Check 'format_stamp' #####
+expect_error(
+  create_path(filename = "abc.txt", format_stamp = character(0),
+              dir = my_tempdir, add_date = TRUE),
+  pattern = "checkinput::is_character(format_stamp, allow_empty = TRUE) is not TRUE", fixed = TRUE)
+
+expect_error(
+  create_path(filename = "abc.txt", format_stamp = NA_character_,
+              dir = my_tempdir, add_date = TRUE),
+  pattern = "checkinput::is_character(format_stamp, allow_empty = TRUE) is not TRUE", fixed = TRUE)
+
+expect_error(
+  create_path(filename = "abc.txt", format_stamp = c("", "%d_%m_%Y"),
+              dir = my_tempdir, add_date = TRUE),
+  pattern = "checkinput::is_character(format_stamp, allow_empty = TRUE) is not TRUE", fixed = TRUE)
+
+##### Check 'dir' #####
+for(dir in list(3, "", character(0), NULL, c("temp_p1", "temp_p2"))) {
+  expect_error(
+    create_path(filename = "abc.txt", dir = dir),
+    pattern = "checkinput::is_character(dir) is not TRUE", fixed = TRUE)
+}
+
+for(dir in list(paste0(my_tempdir, "./"), paste0(my_tempdir, "temp_p1/"))) {
+  expect_error(
+    create_path(filename = "abc.txt", dir = dir),
+    pattern = "'dir' should not end with '/'", fixed = TRUE)
+}
+
+for(dir in list(paste0(my_tempdir, ".\\"), paste0(my_tempdir, "temp_p1\\"))) {
+  expect_error(
+    create_path(filename = "abc.txt", dir = dir),
+    pattern = "'dir' should not end with '\\'", fixed = TRUE)
+}
+
+for(dir in list(paste0(my_tempdir, ".."), paste0(my_tempdir, "temp_p1."))) {
+  expect_error(
+    create_path(filename = "abc.txt", dir = dir),
+    pattern = "'dir' should not end with '.'", fixed = TRUE)
+}
+
+for(dir in list(paste0(my_tempdir, ". "), paste0(my_tempdir, "temp_p1 "))) {
+  expect_error(
+    create_path(filename = "abc.txt", dir = dir),
+    pattern = "'dir' should not end with ' ' (i.e., a space)", fixed = TRUE)
+}
+
+##### Check 'add_date' #####
+for(add_date in list(3, NA)) {
+  expect_error(
+    create_path(filename = "abc.txt", dir = my_tempdir, add_date = add_date),
+    pattern = "checkinput::is_logical(add_date) is not TRUE", fixed = TRUE)
+}
 
 
-
-# create_path(filename = "test1a.txt", format_stamp = "", add_date = TRUE)
-# create_path(filename = "test1b.txt", format_stamp = "%H_%M", add_date = TRUE)
-# create_path(filename = "test1c.txt", format_stamp = "%H_%M_%OS3", add_date = TRUE)
-# create_path(filename = "test2a.html", format_stamp = "", add_date = FALSE)
-# create_path(filename = "test2b.html", format_stamp = "%H_%M", add_date = FALSE)
-# create_path(filename = "test2c.html", format_stamp = "%H_%M_%OS3", add_date = FALSE)
-
-# Filenames that already exist, filenames that are directories
-# ...
-
-
-# my_tempfile <- file.path(tempdir(), "test_df.csv")
-# # Write csv-file, modified from example in help(write.table)
-# write.table(x = data.frame(a = "a", b = pi), file = my_tempfile)
-#
-#
-# dir <- file.path(my_tempdir, "temp_subdirF_dateF")
-# expected_path <- dir
+#### Cleaning up ####
+##### Delete the created temporary files #####
+unlink(
+  x = file.path(my_tempdir,
+                c(".txt",
+                  format(Sys.time(), format = "%Y_%m_%d"),
+                  "subdir",
+                  "test_df.csv")),
+  recursive = TRUE)
 
 
-# # 5 Checks on input to 'dir'
-# for(dir in list(3, "", character(0), NULL, c("temp_p1", "temp_p2"))) {
-#   expect_error(
-#     create_dir(dir = dir),
-#     pattern = "is_character(dir) is not TRUE", fixed = TRUE)
-# }
-#
-# for(dir in list(paste0(my_tempdir, "./"), paste0(my_tempdir, "temp_p1/"))) {
-#   expect_error(
-#     create_dir(dir = dir),
-#     pattern = "'dir' should not end with '/'", fixed = TRUE)
-# }
-#
-# for(dir in list(paste0(my_tempdir, ".\\"), paste0(my_tempdir, "temp_p1\\"))) {
-#   expect_error(
-#     create_dir(dir = dir),
-#     pattern = "'dir' should not end with '\\'", fixed = TRUE)
-# }
-#
-# for(dir in list(paste0(my_tempdir, ".."), paste0(my_tempdir, "temp_p1."))) {
-#   expect_error(
-#     create_dir(dir = dir),
-#     pattern = "'dir' should not end with '.'", fixed = TRUE)
-# }
-#
-# for(dir in list(paste0(my_tempdir, ". "), paste0(my_tempdir, "temp_p1 "))) {
-#   expect_error(
-#     create_dir(dir = dir),
-#     pattern = "'dir' should not end with ' ' (i.e., a space)", fixed = TRUE)
-# }
-#
-# # Check if illegal characters are recognised
-# expect_false(grepl(pattern = "[<]", x = "abc"))
-# expect_false(grepl(pattern = "[>]", x = "abc"))
-# expect_false(grepl(pattern = '["]', x = 'abc'))
-# expect_false(grepl(pattern = "[|]", x = "abc"))
-# expect_false(grepl(pattern = "[?]", x = "abc"))
-# expect_false(grepl(pattern = "[*]", x = "abc"))
-#
-# expect_true(grepl(pattern = "[<]", x = "ab<c"))
-# expect_true(grepl(pattern = "[>]", x = "ab>c"))
-# expect_true(grepl(pattern = '["]', x = 'ab"c'))
-# expect_true(grepl(pattern = "[|]", x = "ab|c"))
-# expect_true(grepl(pattern = "[?]", x = "ab?c"))
-# expect_true(grepl(pattern = "[*]", x = "ab*c"))
-#
-# expect_true(grepl(pattern = '[<>"|?*]', x = "ab<c"))
-# expect_true(grepl(pattern = '[<>"|?*]', x = "ab>c"))
-# expect_true(grepl(pattern = '[<>"|?*]', x = 'ab"c'))
-# expect_true(grepl(pattern = '[<>"|?*]', x = "ab|c"))
-# expect_true(grepl(pattern = '[<>"|?*]', x = "ab?c"))
-# expect_true(grepl(pattern = '[<>"|?*]', x = "ab*c"))
-#
-# # 'dir' points to a file instead of a directory
-# expect_warning(
-#   expect_equal(create_dir(dir = my_tempfile, add_date = FALSE),
-#                normalizePath(getwd())),
-#   pattern = paste0("failed! Returning\nthe working directory"))
-#
-# # 6 Checks on input to 'add_date'
-# for(add_date in list(3, NA)) {
-#   expect_error(
-#     create_dir(dir = my_tempdir, add_date = add_date),
-#     pattern = "is_logical(add_date) is not TRUE", fixed = TRUE)
-# }
-#
-#
-# #### Delete the created temporary files ####
-# unlink(my_tempfile, recursive = TRUE)
-#
-#
-# #### Remove objects used in tests ####
-# rm(add_date, dir, expected_path, my_tempdir, my_tempfile)
+##### Remove objects used in tests #####
+rm(add_date, dir, filenm, filenm_in, filenm_out, ind_filenm, my_tempdir,
+   my_tempfile)
