88cc4a6d95700b09e806a5689c3aefaf33577acd
JesseAlderliesten <jessealderliesten@hotmail.com>
2026-02-26 12:23:56 +0100

    file_path_sans_ext(), replacing tools::file_path_sans_ext(), to also recognise the extension of file names that end in a dot.

---
+++ inst/tinytest/test_file_path_sans_ext.R
@@ -0,0 +1,34 @@
+#### Create objects to use in tests ####
+filename <- "ab..txt"
+
+
+#### Test the examples ####
+expect_silent(
+  expect_identical(
+    tools::file_path_sans_ext(filename),
+    "ab..txt")
+)
+
+expect_silent(
+  expect_identical(
+    tools::file_ext(filename),
+    "txt")
+)
+
+# Nonsense-result
+expect_silent(
+  expect_identical(
+    paste0(tools::file_path_sans_ext(filename), ".", tools::file_ext(filename)),
+    "ab..txt.txt")
+)
+
+# Correct result
+expect_silent(
+  expect_identical(
+    paste0(progutils::file_path_sans_ext(filename), ".", tools::file_ext(filename)),
+    filename)
+)
+
+
+#### Remove objects used in tests ####
+rm(filename)
