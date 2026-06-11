5d3cd4461d05baa63fb52e649b96a8cdd6c7aa4f
JesseAlderliesten <59017087+JesseAlderliesten@users.noreply.github.com>
2026-06-11 16:05:13 +0200

    Use create_tempdir(). Standardise some notation.

--- inst/tinytest/test_get_file_path.R
+++ inst/tinytest/test_get_file_path.R
@@ -2,71 +2,72 @@ tinytest::report_side_effects()
 
 
 #### Test the examples ####
+my_tempdir <- create_tempdir(pattern = "examplegetfilepath")
 my_tempfiles <- fs::path_abs(
-  tempfile(pattern = c("some_filename", "another_filename"), fileext = ".txt")
+  fs::path(my_tempdir, paste0(c("some_filename", "another_filename"), ".txt"))
 )
 
 # Create the files
-expect_silent(
-  expect_equal(file.create(my_tempfiles), rep.int(TRUE, length(my_tempfiles)))
-)
+expect_equal(file.create(my_tempfiles), rep.int(TRUE, length(my_tempfiles)))
+expect_true(all(fs::is_file(my_tempfiles)))
 
 expect_message(
-  expect_equal(basename(get_file_path(dir = tempdir(), pattern = "some_filename")),
-               basename(grep(pattern = "some_filename", x = my_tempfiles,
-                             value = TRUE, fixed = TRUE))),
+  expect_true(
+    endsWith(x = get_file_path(dir = my_tempdir, pattern = "some_filename"),
+             suffix = basename(grep(pattern = "some_filename", x = my_tempfiles,
+                                    value = TRUE, fixed = TRUE)))),
   pattern = "Using file", strict = TRUE, fixed = TRUE)
 
 # The same file is found if case-insensitive matching is used:
 expect_message(
-  expect_equal(basename(get_file_path(dir = tempdir(), pattern = "SOME_FILE",
-                                      ignore_case = TRUE)),
-               grep(pattern = "some_file", x = basename(my_tempfiles),
-                    value = TRUE, fixed = TRUE)),
+  expect_true(
+    endsWith(x = get_file_path(dir = my_tempdir, pattern = "SOME_FILENAME",
+                               ignore_case = TRUE),
+             suffix = basename(grep(pattern = "some_filename", x = my_tempfiles,
+                                    value = TRUE, fixed = TRUE)))),
   pattern = "Using file", strict = TRUE, fixed = TRUE)
 
 expect_error(
-  get_file_path(dir = tempdir(), pattern = "SOME_FILE", ignore_case = FALSE),
-  pattern = basename(my_tempfiles[1])
+  get_file_path(dir = my_tempdir, pattern = "SOME_FILENAME", ignore_case = FALSE),
+  pattern = paste0("case-insensitive match.+", basename(my_tempfiles[1]))
 )
 
 expect_error(
-  get_file_path(dir = tempdir(), pattern = "missing_filename_abcde",
+  get_file_path(dir = my_tempdir, pattern = "missing_filename_abcde",
                 ignore_case = TRUE),
   pattern = "No matches to pattern 'missing_filename_abcde' are present"
 )
 
 expect_error(
-  get_file_path(dir = tempdir(), pattern = "missing_filename_abcde",
+  get_file_path(dir = my_tempdir, pattern = "missing_filename_abcde",
                 ignore_case = FALSE),
   pattern = "No case-sensitive matches to pattern 'missing_filename_abcde' are present"
 )
 
 expect_error(
-  get_file_path(dir = tempdir(), pattern = "missing_filename_abcde", ignore_case = FALSE),
+  get_file_path(dir = my_tempdir, pattern = "missing_filename_abcde", ignore_case = FALSE),
   pattern = "No case-sensitive matches to pattern 'missing_filename_abcde' are present"
 )
 
 expect_error(
-  get_file_path(dir = tempdir(), pattern = "_filename", ignore_case = TRUE),
+  get_file_path(dir = my_tempdir, pattern = "_filename", ignore_case = TRUE),
   pattern = "Multiple matches to pattern '_filename' are present"
 )
 
 expect_error(
-  get_file_path(dir = tempdir(), pattern = "_filename", ignore_case = FALSE),
+  get_file_path(dir = my_tempdir, pattern = "_filename", ignore_case = FALSE),
   pattern = "Multiple case-sensitive matches to pattern '_filename' are present"
 )
 
 # Deleting the created temporary files
-unlink(x = my_tempfiles)
-rm(my_tempfiles)
+unlink(x = my_tempdir, recursive = TRUE)
+rm(my_tempdir, my_tempfiles)
 
 
 #### Tests ####
 # Create files in a temporary directory so we know what is present.
-my_tempdir <- fs::path(tempdir(), "testgetfilepath")
-fs::dir_create(my_tempdir)
-my_tempfile <- fs::path(my_tempdir, "test_df.csv")
+my_tempdir <- create_tempdir(pattern = "testgetfilepath")
+fs::dir_create(path = fs::path(my_tempdir, "test_dir", "abc"))
 
 # Test that temporary subdirectory is writeable by writing a csv-file, modified
 # from an example in help(write.table)
@@ -75,29 +76,28 @@ expect_false(fs::is_file(my_tempfile))
 write.table(x = data.frame(a = "a", b = pi), file = my_tempfile)
 expect_true(fs::is_file(my_tempfile))
 
-
 expect_message(
   expect_true(endsWith(
-    get_file_path(dir = dirname(my_tempfile), pattern = "test_df",
+    get_file_path(dir = my_tempdir, pattern = "test_df",
                   quietly = FALSE),
-    suffix = fs::path("testgetfilepath", "test_df.csv")
+    suffix = fs::path(my_tempdir, "test_df.csv")
   )),
   pattern = "Using file", strict = TRUE, fixed = TRUE)
 
 expect_silent(
   expect_true(endsWith(
-    get_file_path(dir = dirname(my_tempfile), pattern = "test_df",
+    get_file_path(dir = my_tempdir, pattern = "test_df",
                   quietly = TRUE),
-    suffix = fs::path("testgetfilepath", "test_df.csv")
+    suffix = fs::path(my_tempdir, "test_df.csv")
   ))
 )
 
 # Check that 'pattern' is interpreted as a regular expression
 expect_message(
   expect_true(endsWith(
-    get_file_path(dir = dirname(my_tempfile), pattern = "te.t_df",
+    get_file_path(dir = my_tempdir, pattern = "te.t_df",
                   quietly = FALSE),
-    suffix = fs::path("testgetfilepath", "test_df.csv")
+    suffix = fs::path(my_tempdir, "test_df.csv")
   )),
   pattern = "Using file", strict = TRUE, fixed = TRUE)
 
@@ -110,8 +110,7 @@ expect_error(get_file_path(dir = fs::path(my_tempdir, "abc"), pattern = "test_")
              pattern = "Directory does not exist")
 
 # 'pattern' points to an existing directory instead of to an existing file
-fs::dir_create(path = fs::path(tempdir(), "testgetfilepath", "test_dir", "abc"))
-expect_error(get_file_path(dir = fs::path(tempdir(), "testgetfilepath", "test_dir"),
+expect_error(get_file_path(dir = fs::path(my_tempdir, "test_dir"),
                            pattern = "abc"),
              pattern = "No matches to pattern 'abc' are present",
              fixed = TRUE)
@@ -131,7 +130,7 @@ expect_error(get_file_path(dir = my_tempdir, pattern = "test_dir", quietly = 1),
 
 
 #### Delete the created temporary files ####
-unlink(x = dirname(my_tempfile), recursive = TRUE)
+unlink(x = my_tempdir, recursive = TRUE)
 
 
 #### Remove objects used in tests ####
