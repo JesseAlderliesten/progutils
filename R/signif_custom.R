45b65f5f2ea3061a9df15d6c8ed77413da6b6fb9
JesseAlderliesten <59017087+JesseAlderliesten@users.noreply.github.com>
2026-09-18 12:46:49 +0200

    Add function `signif_custom()` to round values while ensuring that their integer part is not rounded.

---
+++ R/signif_custom.R
@@ -0,0 +1,107 @@
+#' Round numbers
+#'
+#' Round numbers to a specified minimum number of significant digits while
+#' ensuring that the integer part is not rounded.
+#'
+#' @param x [numeric vector][checkinput::all_numbers()], [matrix] or
+#' [data.frame], see `Details`.
+#' @param digits the minimum number of [significant digits][signif()] to round
+#' to (rounded to the nearest integer in the range from 0 to 22), see `Details`.
+#' @param type [character string][checkinput::is_character()] `"selective"` or
+#' `"expanded"` to indicate the type of rounding to be used, see `Details`.
+#'
+#' @returns
+#' `x` with rounded values, see `Details`.
+#'
+#' @details
+#' [Rounding][signif()] numbers to a specified number of significant digits can
+#' lead to values ending in rounded zeros, e.g., `"14300"` instead of `"14286"`
+#' when rounding `(1e5)/7` to 3 significant digits. To prevent this,
+#' `signif_custom()` treats the value in argument `digits` as a **minimum**
+#' value and rounds to more significant digits if that is needed to not round
+#' the integer part of numbers.
+#'
+#' If `type` is `"selective"`, the required value of `digits` is determined for
+#' each value individually, such that the number of significant digits might
+#' differ if `x` has length larger than one. If type is `"expanded"`, the value
+#' of `digits` is increased to the same value for all numbers in a vector or
+#' column.
+#'
+#' `signif_custom()` also handles [matrices][matrix] and
+#' [dataframes][data.frame], rounding only their numeric columns.
+#'
+#' @seealso
+#' [signif()] to round to a specified number of significant digits; [zapsmall()]
+#' to put small values to zero; [formatC()] for other ways to format numbers.
+#'
+#' @examples
+#' x1 <- (1e5)/7
+#' signif(x = x1, digits = 3) # returns 14300
+#' signif_custom(x1, digits = 3) # returns 14286 to not round the integer part
+#'
+#' x2 <- c(10^c(-1, 5)/7)
+#' signif(x = x2, digits = 3) # returns c(1.43e-02, 1.43e+04)
+#' # Increase the number of digits only for the
+#' # second number, returning c(0.0143, 14286.0000)
+#' signif_custom(x2, digits = 3, type = "selective")
+#' # Increase the number of digits for both
+#' # numbers, returning c(0.014286, 14286.0000)
+#' signif_custom(x2, digits = 3, type = "expanded")
+#'
+#' x4 <- c(-0.1, 1, -1e4, 1e5) / 7
+#' signif(x = x4, digits = 3)
+#' # returns c(-0.0143,   0.143,   -1430,   14300)
+#' signif_custom(x = x4, digits = 3, type = "selective")
+#' # returns c(-0.0143,   0.1430,  -1429.0, 14286)
+#' signif_custom(x = x4, digits = 3, type = "expanded")
+#' # returns c(-0.014286, 0.14286, -1428.6, 14286)
+#'
+#' x_df <- data.frame(a = 1:3, b = letters[11:13], c = c(1e5, 1e3, 1)/7, d = pi)
+#' signif_custom(x_df, type = "selective")
+#' signif_custom(x_df, type = "expanded")
+#'
+#' @export
+signif_custom <- function(x, digits = 3L, type = c("selective", "expanded")) {
+  type <- match.arg(type, several.ok = FALSE)
+
+  # Notes:
+  # - Values in 'digits' are rounded to the nearest integer in the range from 0
+  #   to 22 in signif(), so no need to check if 'digits' are nonnegative and
+  #   integer.
+  stopifnot(checkinput::is_number(digits))
+
+  if(!is.null(nrow(x))) {
+    if(is.matrix(x)) {
+      ind_cols_numeric <- which(apply(X = x, MARGIN = 2L, FUN = is.numeric))
+    } else {
+      ind_cols_numeric <- which(
+        unlist(lapply(X = x, FUN = is.numeric), use.names = FALSE))
+    }
+
+    for(ind_col in ind_cols_numeric) {
+      x[, ind_col] <- signif_custom(x = x[, ind_col], digits = digits, type = type)
+    }
+    return(x)
+  } else {
+    stopifnot(is.numeric(x))
+  }
+
+  # Using abs(x) to also work if x contains negative values.
+  ceiling_digits_x <- ceiling(log10(abs(x)))
+
+  if(type == "expanded") {
+    digits <- max(digits, ceiling_digits_x)
+  } else {
+    digits_p <- pmax(digits, ceiling_digits_x)
+
+    # numeric(0) in 'x' propagates to zero-length numeric(0) in
+    # ceiling_digits_x and in digits_p, leading to an error in signif().
+    # To circumvent this, argument 'digits' is used instead of digits_p if
+    # digits_p has a length of zero.
+    if(length(digits_p) > 0) {
+      digits <- digits_p
+    }
+  }
+
+  signif(x = x, digits = digits)
+}
---
+++ inst/tinytest/test_signif_custom.R
@@ -0,0 +1,192 @@
+#### signif_custom ####
+x1 <- c(-0.1, 1) / 7
+x2 <- c(-1e4, 1e5) / 7
+x12 <- c(x1, x2)
+x_exp <- 10^c(-2, -4, 2, 4) / 7
+x_exp <- c(-1 * x_exp, x_exp)
+
+x1_df <- as.data.frame(x1)
+x2_df <- as.data.frame(x2)
+x12_df <- as.data.frame(x12)
+x_exp_df <- as.data.frame(x_exp)
+x1_df_cols <- as.data.frame(rbind(x1))
+x12_df_cols <- as.data.frame(cbind(x1_df, x2_df))
+
+x1_mat <- as.matrix(x1)
+x2_mat <- as.matrix(x2)
+x12_mat <- as.matrix(x12)
+x_exp_mat <- as.matrix(x_exp_df)
+x1_mat_cols <- rbind(x1)
+x12_mat_cols <- as.matrix(cbind(x1_df, x2_df))
+
+x_df <- data.frame(a = 1:3, b = letters[11:13], c = c(1e5, 1e3, 1)/7, d = pi)
+expect_equal(
+  signif_custom(x_df, type = "selective"),
+  data.frame(a = 1:3, b = letters[11:13], c = c(14286, 143, 0.143),
+             d = rep(3.14, 3L))
+)
+expect_equal(
+  signif_custom(x_df, type = "expanded"),
+  data.frame(a = 1:3, b = letters[11:13], c = c(14286, 142.86, 0.14286),
+             d = rep(3.14, 3L))
+)
+
+for(type in c("selective", "expanded")) {
+  expect_silent(
+    expect_equal(
+      signif_custom(x = x1, digits = 3, type = type),
+      c(-0.0143, 0.143),
+      info = paste0("1 (abs(x) < 10^digits, type '", type, "')")
+    )
+  )
+
+  expect_equal(
+    signif_custom(x = x1_df, digits = 3, type = type),
+    data.frame(x1 = c(-0.0143, 0.143)),
+    info = paste0("1_df (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_x1_df_cols <- data.frame(rbind(x1 = c(V1 = -0.0143, V2 = 0.143)))
+  expect_equal(
+    signif_custom(x = x1_df_cols, digits = 3, type = type),
+    expect_x1_df_cols,
+    info = paste0("1_df_cols (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x1_mat, digits = 3, type = type),
+    unname(as.matrix(data.frame(c(-0.0143, 0.143)))),
+
+    info = paste0("1_mat (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x1_mat_cols, digits = 3, type = type),
+    t(as.matrix(data.frame(x1 = c(-0.0143, 0.143)))),
+    info = paste0("1_mat_cols (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  if(type == "selective") {
+    expect_x2 <- c(-1429, 14286)
+  } else {
+    expect_x2 <- c(-1428.6, 14286)
+  }
+
+  expect_equal(
+    signif_custom(x = x2, digits = 3, type = type),
+    expect_x2,
+    info = paste0("2 (abs(x) > 10^digits, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x2_df, digits = 3, type = type),
+    data.frame(x2 = expect_x2),
+    info = paste0("2_df (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x2_mat, digits = 3, type = type),
+    unname(as.matrix(data.frame(expect_x2))),
+    info = paste0("2_mat (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  if(type == "selective") {
+    expect_x12 <- c(-0.0143, 0.143, -1429, 14286)
+  } else {
+    expect_x12 <- c(-0.014286, 0.14286, -1428.6, 14286)
+  }
+
+  expect_equal(
+    signif_custom(x = x12, digits = 3, type = type),
+    expect_x12,
+    info = paste0("3 (some abs(x) > 10^digits, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x12_df, digits = 3, type = type),
+    data.frame(x12 = expect_x12),
+    info = paste0("3_df (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_x12_df_cols <- data.frame(x1 = t(unname(expect_x1_df_cols)),
+                                   x2 = unname(cbind(expect_x2)))
+  expect_equal(
+    signif_custom(x = x12_df_cols, digits = 3, type = type),
+    expect_x12_df_cols,
+    info = paste0("3_df_cols (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x12_mat, digits = 3, type = type),
+    as.matrix(expect_x12),
+    info = paste0("3_mat (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x12_mat_cols, digits = 3, type = type),
+    as.matrix(expect_x12_df_cols),
+    info = paste0("3_mat_cols (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  if(type == "selective") {
+    expect_exp <- c(-0.00143, -1.43e-05, -14.3, -1429,
+                    0.00143,  1.43e-05,  14.3,  1429)
+  } else {
+    expect_exp <- signif(x = x_exp, digits = 4)
+  }
+
+  expect_equal(
+    signif_custom(x = x_exp, digits = 3, type = type),
+    expect_exp,
+    info = paste0("4 (some abs(x) larger than 10^digits, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x_exp_df, digits = 3, type = type),
+    data.frame(x_exp = expect_exp),
+    info = paste0("4_df (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_exp_mat <- as.matrix(x = expect_exp)
+  colnames(expect_exp_mat) <- "x_exp"
+  expect_equal(
+    signif_custom(x = x_exp_mat, digits = 3, type = type),
+    expect_exp_mat,
+    info = paste0("4_mat (abs(x) < 10^digits, type '", type, "')")
+  )
+
+  expect_error(
+    signif_custom(x = NULL, type = type),
+    pattern = "is.numeric(x) is not TRUE", fixed = TRUE,
+    info = paste0("5 (error for zero-length 'x', type '", type, "')")
+  )
+
+  expect_error(
+    signif_custom(x = 0.01 / 7, digits = integer(0), type = type),
+    pattern = "is_number(digits) is not TRUE", fixed = TRUE,
+    info = paste0("6 (error for zero-length 'digits', type '", type, "')")
+  )
+
+  if(type == "selective") {
+    expect_t7 <- c(-1e-3, -1e-5, -14, -1429, 1e-3, 1e-5, 14, 1429)
+  } else {
+    expect_t7 <- signif(x = x_exp, digits = 4)
+  }
+
+  expect_equal(
+    signif_custom(x = x_exp, digits = -3, type = type),
+    expect_t7,
+    info = paste0("7 (negative values for 'digits' are set to 0, type '", type, "')")
+  )
+
+  expect_equal(
+    signif_custom(x = x_exp_df, digits = -3, type = type),
+    data.frame(x_exp = expect_t7),
+    info = paste0("7_df (abs(x) < 10^digits, type '", type, "')")
+  )
+}
+
+rm(expect_exp, expect_exp_mat, expect_t7, expect_x2, expect_x12_df_cols,
+   expect_x12, expect_x1_df_cols, type, x1, x1_df, x1_df_cols,
+   x1_mat, x1_mat_cols, x2, x2_df, x2_mat, x12, x12_df, x12_df_cols, x12_mat,
+   x12_mat_cols, x_df, x_exp, x_exp_df, x_exp_mat)
